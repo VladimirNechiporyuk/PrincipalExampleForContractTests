ff150f68593c829e338eb485441f38a4bd8a40a0
Vladimir Nechiporyuk <magicfess@gmail.com>
2019-12-02 15:08:39 +0200

    Init simple security

--- src/main/java/com/example/principalexampleforcontracttests/config/swagger/SwaggerConfig.java
+++ src/main/java/com/example/principalexampleforcontracttests/config/swagger/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.example.principalexampleforcontracttests.config;
+package com.example.principalexampleforcontracttests.config.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
